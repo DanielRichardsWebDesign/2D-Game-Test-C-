6822b58b18a9aa3c866bd2684bc0c939e3e636f0
Daniel Richards <40298831+DanielRichardsWebDesign@users.noreply.github.com>
2020-07-11 15:10:47 +0100

    Installed SFML libraries and added dependencies with them to project.

--- 2D Game Test C++/Main.cpp
+++ 2D Game Test C++/Main.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+//Include SFML Libraries
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
 
 int main()
 {
