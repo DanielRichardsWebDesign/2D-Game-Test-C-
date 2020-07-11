646a4c856b379e8f035646470677e6bfc3eda718
Daniel Richards <40298831+DanielRichardsWebDesign@users.noreply.github.com>
2020-07-11 15:37:22 +0100

    Created Window. Now displays blue background.

--- 2D Game Test C++/Main.cpp
+++ 2D Game Test C++/Main.cpp
@@ -8,6 +8,41 @@
 
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	//Window Logic
+	sf::RenderWindow window(sf::VideoMode(640, 480), "2D Game Test", sf::Style::Titlebar | sf::Style::Close);
+	sf::Event ev;
+
+	//Game Loop
+	while (window.isOpen())
+	{
+		//Event Polling
+		while (window.pollEvent(ev))
+		{
+			switch (ev.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::KeyPressed:
+				if (ev.key.code == sf::Keyboard::Escape)
+					window.close();
+				break;
+			}
+		}
+		//Update
+
+		
+		
+		//Render
+		window.clear(sf::Color::Blue); //Clears old frame
+
+		//Draw Game
+
+
+		window.display(); //Tell application that the window is done drawing
+
+	}
+
+	//End of Application
 	return 0;
 }
